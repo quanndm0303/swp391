f88e09b5b380ddb2e19f321b38fcab1e2f575b72
Quangz <quang@kobizo.de>
2024-05-17 12:02:09 +0700

    first commit

---
+++ src/test/java/com/app/zware/ZwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.zware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
