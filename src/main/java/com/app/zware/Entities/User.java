61c3803171e4a16af2f6a3580e9bb36c4bef4df5
Quangz <quang@kobizo.de>
2024-05-17 13:32:54 +0700

    db & User entity

---
+++ src/main/java/com/app/zware/Entities/User.java
@@ -0,0 +1,33 @@
+package com.app.zware.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    private String name;
+    private String role;
+    private String phone;
+    private String gender;
+    private String avatar;
+
+    private Date dateOfBirth;
+
+}
+
