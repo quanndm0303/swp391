334c44f884c063b7ab7fb0f4f0966cd0db567c6a
CaoTrongHai <tronghaipdp@gmail.com>
2024-05-24 07:48:37 +0700

    update WarehouseZone and CRUD Category

--- src/main/java/com/app/zware/Util/WarehouseZoneService.java
+++ src/main/java/com/app/zware/Util/WarehouseZoneService.java
@@ -15,4 +15,26 @@ public class WarehouseZoneService {
     public List<WarehouseZone> getAll(){
         return warehouseZoneRespository.findAll();
     }
+    public WarehouseZone createWarehouseZone (WarehouseZone request){
+        WarehouseZone warehouseZone = new WarehouseZone();
+        warehouseZone.setName(request.getName());
+        return warehouseZoneRespository.save(warehouseZone);
+    }
+    public WarehouseZone getWarehouseZoneById(int id){
+      return warehouseZoneRespository.findById(id).orElseThrow(()->new RuntimeException("Not Found WarehouseZone"));
+    }
+    public void deleteWarehouseZoneById(int id){
+         warehouseZoneRespository.deleteById(id);
+    }
+    public boolean checkIdExist(int id){
+        return  warehouseZoneRespository.existsById(id);
+
+    }
+    public WarehouseZone updateWarehouseZone(int id,WarehouseZone request){
+        WarehouseZone warehouseZone = getWarehouseZoneById(id);
+        if(request.getName()!=null){
+            warehouseZone.setName(request.getName());
+        }
+        return warehouseZoneRespository.save(warehouseZone);
+    }
 }
