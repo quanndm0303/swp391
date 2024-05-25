fe5d37bd1baee2354cad44ea00663dd8c2efbf71
QuanNguyen <nguyendinhminhquan03@gmail.com>
2024-05-25 18:05:43 +0700

    crud Product(Code optimization)

--- src/main/java/com/app/zware/Util/ProductService.java
+++ src/main/java/com/app/zware/Util/ProductService.java
@@ -2,15 +2,13 @@ package com.app.zware.Util;
 
 
 import com.app.zware.Entities.Product;
-import com.app.zware.Entities.User;
-import com.app.zware.Entities.Warehouse;
 import com.app.zware.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProductService {
@@ -26,12 +24,12 @@ public class ProductService {
     }
 
     public Product createProduct(Product request) {
-        Product product = new Product();
-        product.setName(request.getName());
-        product.setSupplier(request.getSupplier());
-        product.setCategory_id(request.getCategory_id());
-        product.setMeasure_unit(request.getMeasure_unit());
-        return productRepository.save(product);
+//        Product product = new Product();
+//        product.setName(request.getName());
+//        product.setSupplier(request.getSupplier());
+//        product.setCategory_id(request.getCategory_id());
+//        product.setMeasure_unit(request.getMeasure_unit());
+        return productRepository.save(request);
     }
 
     public void deleteProductById(int id) {
