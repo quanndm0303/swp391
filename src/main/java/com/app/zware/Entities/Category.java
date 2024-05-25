6517ff89c0f3b165b68fcc107ba3c5d8c12da495
Quangz <quang@kobizo.de>
2024-05-25 14:31:25 +0700

    edit entities: no relationship

--- src/main/java/com/app/zware/Entities/Category.java
+++ src/main/java/com/app/zware/Entities/Category.java
@@ -4,20 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 
 @Entity(name = "categories")
+@Data
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String name;
 }
