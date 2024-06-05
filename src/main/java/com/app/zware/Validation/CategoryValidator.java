3e62c263a8ded2ac67fc917b8931714864c95d33
QuanNguyen <nguyendinhminhquan03@gmail.com>
2024-06-05 11:17:44 +0700

    validation categories

---
+++ src/main/java/com/app/zware/Validation/CategoryValidator.java
@@ -0,0 +1,46 @@
+package com.app.zware.Validation;
+
+import com.app.zware.Entities.Category;
+import com.app.zware.Repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CategoryValidator {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public String checkPost(Category category) {
+        if (category.getName().isBlank()) {
+            return "Category name is invalid";
+        }
+        Optional<Category> checkNameExist = categoryRepository.findByName(category.getName());
+        if (checkNameExist.isPresent()) {
+            return "Category name is exist";
+        }
+        return "";
+    }
+
+    public String checkPut(Category category) {
+        return checkPost(category);
+    }
+
+    public boolean checkCategoryId(Integer id){
+        return categoryRepository.existsById(id);
+    }
+
+    public String checkGet(Integer categoryId){
+        if(!checkCategoryId(categoryId)){
+            return "Not Found Category ID";
+        }
+        return "";
+    }
+
+    public String checkDelete(Integer categoryId){
+        return checkGet(categoryId);
+    }
+
+}
+
