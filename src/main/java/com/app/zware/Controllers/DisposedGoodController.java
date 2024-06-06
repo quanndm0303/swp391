a4b1258cd36f2aa3ecf491299c5c6cc86f5a414c
CaoTrongHai <tronghaipdp@gmail.com>
2024-06-06 07:03:39 +0700

    validate-disposedGood

--- src/main/java/com/app/zware/Controllers/DisposedGoodController.java
+++ src/main/java/com/app/zware/Controllers/DisposedGoodController.java
@@ -2,6 +2,7 @@ package com.app.zware.Controllers;
 
 import com.app.zware.Entities.DisposedGood;
 import com.app.zware.Service.DisposedGoodService;
+import com.app.zware.Validation.DisposedGoodValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,9 @@ public class DisposedGoodController {
     @Autowired
     DisposedGoodService disposedGoodService;
 
+    @Autowired
+    DisposedGoodValidator disposedGoodValidator;
+
     @GetMapping("")
     public ResponseEntity<?> index() {
         List<DisposedGood> disposedGoodList = disposedGoodService.getAllDisposedGood();
@@ -28,37 +32,52 @@ public class DisposedGoodController {
 
     @PostMapping("")
     public ResponseEntity<?> store(@RequestBody DisposedGood disposedGood) {
-        return new ResponseEntity<>(disposedGoodService.createDisposedGood(disposedGood), HttpStatus.OK);
+       String checkMessage = disposedGoodValidator.checkPost(disposedGood);
+       if(!checkMessage.isEmpty()){
+           return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
+       }
+       disposedGoodService.createDisposedGood(disposedGood);
+       return new ResponseEntity<>("Disposed has been created successfully",HttpStatus.OK);
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<?> show(@PathVariable("id") Integer id) {
-        try {
-            DisposedGood disposedGood = disposedGoodService.getDisposedGoodById(id);
-            return new ResponseEntity<>(disposedGood, HttpStatus.OK);
+      String checkMessage = disposedGoodValidator.checkGet(id);
+      if(!checkMessage.isEmpty()){
+          return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
+      }
 
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>("DisposedGoods not found", HttpStatus.NOT_FOUND);
-        }
+      return new ResponseEntity<>(disposedGoodService.getDisposedGoodById(id),HttpStatus.OK);
     }
+
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> destroy(@PathVariable("id") Integer id) {
-        if (!disposedGoodService.checkIdExist(id)) {
-            return new ResponseEntity<>("Not Found DisposedGoods", HttpStatus.NOT_FOUND);
-        } else {
-            disposedGoodService.deleteDisposedGood(id);
-            return new ResponseEntity<>("DisposedGoods has been deleted successfully", HttpStatus.OK);
+       String checkMessgae = disposedGoodValidator.checkDelete(id);
+           if(!checkMessgae.isEmpty()){
+               return new ResponseEntity<>(checkMessgae,HttpStatus.BAD_REQUEST);
+           }
+           disposedGoodService.deleteDisposedGood(id);
+           return new ResponseEntity<>("Disposal has been deleted successfully",HttpStatus.OK);
         }
-    }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody DisposedGood request) {
-        if (!disposedGoodService.checkIdExist(id)) {
-            return new ResponseEntity<>("Not Found DisposedGoods", HttpStatus.NOT_FOUND);
-        } else {
-            DisposedGood disposedGood = disposedGoodService.updateDisposedGoodById(id, request);
-            return new ResponseEntity<>(disposedGood, HttpStatus.OK);
-        }
+          DisposedGood mergeDisposedGood = disposedGoodService.merge(id,request);
+
+
+          String checkMessage = disposedGoodValidator.checkPut(id,mergeDisposedGood);
+            if(!checkMessage.isEmpty()){
+                return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
+            }
+
+          //update
+        DisposedGood updated = disposedGoodService.update(mergeDisposedGood);
+
+            return new ResponseEntity<>(updated,HttpStatus.OK);
+
     }
 }
---
+++ src/main/java/com/app/zware/Validation/DisposedGoodValidator.java
@@ -0,0 +1,78 @@
+package com.app.zware.Validation;
+
+import com.app.zware.Entities.DisposedGood;
+import com.app.zware.Entities.GoodsDisposal;
+import com.app.zware.Entities.Item;
+import com.app.zware.Repositories.DisposedGoodRespository;
+import com.app.zware.Repositories.GoodsDisposalRepository;
+import com.app.zware.Repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DisposedGoodValidator {
+
+    @Autowired
+    DisposedGoodRespository disposedGoodRespository;
+
+    @Autowired
+    GoodsDisposalRepository goodsDisposalRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public String checkPost(DisposedGood disposedGood){
+         if(disposedGood.getReason().isEmpty()){
+             return "Reason is not empty";
+         }
+         if(disposedGood.getQuantity()==null){
+            return "Quantity is not empty";
+         }
+
+         if(disposedGood.getQuantity()<0){
+             return "Quantity must be greater than 0";
+         }
+
+         if(!checkItemExist(disposedGood.getItem_id())){
+             return "Item Id is not valid";
+         }
+
+         if(!checkGoodsDisposalExist(disposedGood.getDisposal_id())){
+             return "Disposal Id is not valid";
+         }
+         return "";
+        }
+
+    public String checkGet(Integer id){
+        if(!checkIdExist(id)){
+            return "Id is not valid";
+        }
+        return "";
+    }
+
+    public String checkDelete(Integer id){
+        return checkGet(id);
+    }
+
+    public String checkPut(Integer disposedId,DisposedGood disposedGood){
+        if(disposedGood==null||!disposedGoodRespository.existsById(disposedId)){
+            return "Id is not valid";
+        }
+        return checkPost(disposedGood);
+    }
+
+    private boolean checkItemExist (Integer id){
+        return itemRepository.existsById(id);
+    }
+
+    private boolean checkGoodsDisposalExist(Integer id){
+        return goodsDisposalRepository.existsById(id);
+    }
+
+    private boolean checkIdExist(Integer id){
+        return disposedGoodRespository.existsById(id);
+    }
+
+
+    }
+
