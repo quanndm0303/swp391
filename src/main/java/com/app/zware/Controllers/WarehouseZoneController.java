21121553767719af43f0a30fbe76de6f6798acdf
Quangz <quang@kobizo.de>
2024-06-20 15:23:29 +0700

    edit ZoneController response

--- src/main/java/com/app/zware/Controllers/WarehouseZoneController.java
+++ src/main/java/com/app/zware/Controllers/WarehouseZoneController.java
@@ -2,12 +2,12 @@ package com.app.zware.Controllers;
 
 import com.app.zware.Entities.User;
 import com.app.zware.Entities.WarehouseZone;
+import com.app.zware.HttpEntities.CustomResponse;
 import com.app.zware.Service.UserService;
 import com.app.zware.Service.WarehouseZoneService;
-import java.util.List;
-
 import com.app.zware.Validation.WarehouseZoneValidator;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,94 +35,121 @@ public class WarehouseZoneController {
 
   @GetMapping("")
   public ResponseEntity<?> index() {
+    //response
+    CustomResponse customResponse = new CustomResponse();
+
     //Authorization : ALL
 
+    //finally
     List<WarehouseZone> warehouseZones = warehouseZoneService.getAll();
-
-    if (warehouseZones.isEmpty()) {
-      return new ResponseEntity<>("Empty WarehouseZones", HttpStatus.NOT_FOUND);
-    }
-    return new ResponseEntity<>(warehouseZoneService.getAll(), HttpStatus.OK);
+    customResponse.setAll(true, "Get warehouse zones success", warehouseZones);
+    return new ResponseEntity<>(customResponse, HttpStatus.OK);
   }
 
 
   @PostMapping("")
-  public ResponseEntity<?> store(@RequestBody WarehouseZone warehouseZone, HttpServletRequest request) {
-    //Authorization : Admin
+  public ResponseEntity<?> store(@RequestBody WarehouseZone warehouseZone,
+      HttpServletRequest request) {
+    //response
+    CustomResponse customResponse = new CustomResponse();
 
+    //Authorization : Admin
     User user = userService.getRequestMaker(request);
-    if(!user.getRole().equals("admin")){
-      return new ResponseEntity<>("Unauthorized",HttpStatus.UNAUTHORIZED);
+    if (!user.getRole().equals("admin")) {
+      customResponse.setAll(false, "You are not allowed", null);
+      return new ResponseEntity<>(customResponse, HttpStatus.UNAUTHORIZED);
     }
 
+    //finally
     String checkMessage = warehouseZoneValidator.checkPost(warehouseZone);
-    if(!checkMessage.isEmpty()){
-      return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
-    }else {
-      warehouseZoneService.createWarehouseZone(warehouseZone);
-      return new ResponseEntity<>("WarehouseZone has been created successful",HttpStatus.OK);
+    if (!checkMessage.isEmpty()) {
+      customResponse.setAll(false, checkMessage, null);
+      return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
+    } else {
+      WarehouseZone newZone = warehouseZoneService.createWarehouseZone(warehouseZone);
+      customResponse.setAll(true, "Create warehouse zone success", newZone);
+      return new ResponseEntity<>(newZone, HttpStatus.OK);
     }
   }
 
 
   @GetMapping("/{warehouseZoneId}")
   public ResponseEntity<?> show(@PathVariable("warehouseZoneId") int warehouseZoneId) {
+    //response
+    CustomResponse customResponse = new CustomResponse();
+
     //Authorization : ALL
 
-    String checkMessage= warehouseZoneValidator.checkGet(warehouseZoneId);
-    if(!checkMessage.isEmpty()){
-      return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
-    }else {
-      return new ResponseEntity<>(warehouseZoneService.getWarehouseZoneById(warehouseZoneId),HttpStatus.OK);
+    //validation
+    String checkMessage = warehouseZoneValidator.checkGet(warehouseZoneId);
+    if (!checkMessage.isEmpty()) {
+      customResponse.setAll(false, checkMessage, null);
+      return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
     }
 
+    //finally
+    WarehouseZone foundZone = warehouseZoneService.getWarehouseZoneById(warehouseZoneId);
+    customResponse.setAll(true, "Get warehouse zone success", foundZone);
+
+    return new ResponseEntity<>(customResponse, HttpStatus.OK);
   }
 
 
-
   @DeleteMapping("/{warehouseZoneId}")
-  public ResponseEntity<?> destroy(@PathVariable("warehouseZoneId") int warehouseZoneId,HttpServletRequest request) {
+  public ResponseEntity<?> destroy(@PathVariable("warehouseZoneId") int warehouseZoneId,
+      HttpServletRequest request) {
+    //response
+    CustomResponse customResponse = new CustomResponse();
+
     //Authorization : Admin
-
     User user = userService.getRequestMaker(request);
-    if(!user.getRole().equals("admin")){
-      return new ResponseEntity<>("Unauthorized",HttpStatus.UNAUTHORIZED);
+    if (!user.getRole().equals("admin")) {
+      customResponse.setAll(false, "You are not allowed", null);
+      return new ResponseEntity<>(customResponse, HttpStatus.UNAUTHORIZED);
     }
 
+    //Validation
     String checkMessage = warehouseZoneValidator.checkDelete(warehouseZoneId);
-    if(!checkMessage.isEmpty()){
-      return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
-    }else {
-      warehouseZoneService.deleteWarehouseZoneById(warehouseZoneId);
-      return new ResponseEntity<>("WarehouseZone has been deleted successfully",HttpStatus.OK);
+    if (!checkMessage.isEmpty()) {
+      customResponse.setAll(false, checkMessage, null);
+      return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
     }
+
+    //finally
+    warehouseZoneService.deleteWarehouseZoneById(warehouseZoneId);
+    customResponse.setAll(true, "Delete warehouse zone success", null);
+    return new ResponseEntity<>(customResponse, HttpStatus.OK);
 
 
   }
 
   @PutMapping("/{warehouseZoneId}")
   public ResponseEntity<?> update(@PathVariable int warehouseZoneId,
-      @RequestBody WarehouseZone requestWarehouseZone,HttpServletRequest request) {
+      @RequestBody WarehouseZone requestWarehouseZone, HttpServletRequest request) {
+    //response
+    CustomResponse customResponse = new CustomResponse();
+
     //Authorization : Admin
-
     User user = userService.getRequestMaker(request);
-    if(!user.getRole().equals("admin")){
-      return new ResponseEntity<>("Unauthorized",HttpStatus.UNAUTHORIZED);
-    }
-    
-
-    WarehouseZone mergedWarehouseZone = warehouseZoneService.merger(warehouseZoneId,requestWarehouseZone);
-
-    //Validate
-    String checkMessage = warehouseZoneValidator.checkPut(warehouseZoneId,mergedWarehouseZone);
-    if(!checkMessage.isEmpty()){
-      return new ResponseEntity<>(checkMessage,HttpStatus.BAD_REQUEST);
+    if (!user.getRole().equals("admin")) {
+      customResponse.setAll(false, "You are not allowed", null);
+      return new ResponseEntity<>(customResponse, HttpStatus.UNAUTHORIZED);
     }
 
+    //Validation
+    WarehouseZone mergedWarehouseZone = warehouseZoneService.merger(warehouseZoneId,
+        requestWarehouseZone);
+
+    String checkMessage = warehouseZoneValidator.checkPut(warehouseZoneId, mergedWarehouseZone);
+    if (!checkMessage.isEmpty()) {
+      customResponse.setAll(false, checkMessage, null);
+      return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
+    }
 
     //Update
     WarehouseZone updated = warehouseZoneService.update(mergedWarehouseZone);
-    return new ResponseEntity<>(updated,HttpStatus.OK);
+    customResponse.setAll(true, "Update warehouse zone success", updated);
+    return new ResponseEntity<>(customResponse, HttpStatus.OK);
   }
 
 }
